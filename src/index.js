fe79af0ebe4641c72c5b7b530c3fd8e535223b51
Elvis Douglas Janegitz <edjelvis@gmail.com>
2019-04-14 17:14:19 -0300

    Criando middlewares e transformando o App em um container

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import middlewares from './middlewares'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(reducer, middlewares)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root')
+)

180a482b0e998f3d1b869129c109782cc34cd8fa
Elvis Douglas Janegitz <edjelvis@gmail.com>
2019-04-14 18:57:57 -0300

    Criando interface dos Tweets

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
+import LoadingBar from 'react-redux-loading'
 
 class App extends Component {
-
   componentDidMount() {
     this.props.dispatch(handleInitialData())
   }
-
   render() {
     return (
       <div>
-        Starter Code
+        <LoadingBar />
+        {this.props.loading === true
+          ? null
+          : <Dashboard />}
       </div>
     )
   }
 }
 
-export default connect()(App)
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import tweets from './tweets'
 import authedUser from './authedUsers'
 import users from './users'
+import { loadingBarReducer } from 'react-redux-loading'
 
 export default combineReducers({
   authedUser,
   tweets,
-  users
+  users,
+  loadingBar: loadingBarReducer
 })
